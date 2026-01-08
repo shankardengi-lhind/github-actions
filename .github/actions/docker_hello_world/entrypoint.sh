89b48301c49a2a62d12acc7e6557ab6b9e1de957
shankar-lhind <shankar.dengi.extern@munich-airport.de>
2026-01-08 16:09:39 +0100

    commit

--- .github/actions/docker_hello_world/entrypoint.sh
+++ .github/actions/docker_hello_world/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo "$1"
 echo "starting entrypoint shell script"
-echo ${INPUT_greet_hello}
+echo "input_greet_hello:$INPUT_greet_hello"
 date=$(date)
-echo "date=${date}" >> $GITHUB_OUTPUT
+echo "date=$date" >> $GITHUB_OUTPUT
+echo "Hello_time=$date" >> $GITHUB_ENV
 echo "finished entrypoint shell script"
