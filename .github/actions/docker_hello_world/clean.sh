04eef5c542bfa574ec1a68d81d6a5c279a936573
shankar-lhind <shankar.dengi.extern@munich-airport.de>
2026-01-08 16:47:43 +0100

    commit

---
+++ .github/actions/docker_hello_world/clean.sh
@@ -0,0 +1,3 @@
+!#/bin/sh
+echo "Cleaning up..."
+echo " displaying envrionemnt $Hello
--- .github/actions/docker_hello_world/entrypoint1.sh
+++ .github/actions/docker_hello_world/entrypoint1.sh
@@ -5,4 +5,5 @@ echo "input_greet_hello:$INPUT_greet_hello"
 date=$(date)
 echo "date=$date" >> $GITHUB_OUTPUT
 echo "Hello_time=$date" >> $GITHUB_ENV
+echo " print env variable Hello_time : $Hello"
 echo "finished entrypoint shell script"
